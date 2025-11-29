f1aef4842d2993c3f4aaa22425043f9de7c20aaf
diya-thabet <mohamaddhia@gmail.com>
2025-11-29 18:04:52 +0100

    [FIX]: now its working state

--- react/src/components/Footer/Footer.js
+++ react/src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
-import { Email, GitHub, LinkedIn } from "@material-ui/icons";
 import React from "react";
 import FooterCSS from "./Footer.module.css";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
@@ -8,19 +10,22 @@ const Footer = () => {
       <div className={FooterCSS.footercontainer}>
         <h1>Dhia Thabet | 2025</h1>
         <div className={FooterCSS.icons}>
-          {" "}
           <a
             href="https://www.linkedin.com/in/dhiathabet"
             target="_blank"
             rel="noreferrer"
           >
-            <LinkedIn className={FooterCSS.icon} />
+            <LinkedInIcon className={FooterCSS.icon} />
           </a>
-          <a href="https://github.com/diya-thabet" target="_blank" rel="noreferrer">
-            <GitHub className={FooterCSS.icon} />
+          <a
+            href="https://github.com/diya-thabet"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon className={FooterCSS.icon} />
           </a>
           <a href="mailto:mohamaddhia@gmail.com">
-            <Email className={FooterCSS.icon} />
+            <EmailIcon className={FooterCSS.icon} />
           </a>
         </div>
       </div>
